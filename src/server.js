558f011785b520a00fc8c40cd1b618048c62b509
신상우 <sangwooshin@sinsang-us-MacBook-Pro.local>
2024-09-03 23:24:08 +0900

    first commit

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+//const passport = require('passport');
+//const connectDB = require('./config/database');
+//var cors=require('cors');
+
+const app = express();
+//app.use(cors());
+//app.use('/uploads', express.static(__dirname + '/uploads'));
+//app.use(passport.initialize());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB().then(() => {
+  // Start server if database connection is successful
+  app.listen(8080, () => {
+    console.log('Server started on port 8080');
+  });
+}).catch((err) => {
+  console.error('Failed to connect to MongoDB:', err);
+  process.exit(1); // Exit process with failure
+});
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
